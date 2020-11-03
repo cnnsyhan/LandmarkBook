b7b3c9f6c7d2c476a71d48be0ccf0c540c4bf502
Can Seyhan <muhamedseyhan503@gmail.com>
2020-11-03 16:21:42 +0300

    Full Project

---
+++ LandmarkBook/imageViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class imageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var landmarkLabel: UILabel!
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+        
+    }
+    
+
+
+}
